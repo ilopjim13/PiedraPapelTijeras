56c807f816e476bb199159511f64a20da1f6baf8
ivanjim13 <ivangorrion57@gmail.com>
2024-12-11 03:05:19 +0100

    terminado la fase 1

--- PiedraPapelTijeras.cs
+++ PiedraPapelTijeras.cs
@@ -1,43 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+namespace PiedraPapelTijeras;
 
 class PiedraPapelTijeras
 {
-    private int v1 = 0;
-    private int v2 = 0;
     
-    public string texto1 = "";
-    public string texto2 = "";
+    static int v1 = 0;
+    static int v2 = 0;
+
+    static string texto1 = "";
+    static string texto2 = "";
     
     
     static void Main() 
     {
-        Thread t1 = new Thread(new ThreadStart (Go));
-        Thread t2 = new Thread(new ThreadStart (Go2));
-
-        for (int i = 0; i < 3; i++)
+        for (var i = 0; i < 3; i++)
         {
+            var t1 = new Thread(new ThreadStart (Go));
+            var t2 = new Thread(new ThreadStart (Go2));
+            Console.WriteLine($"Ronda {i + 1}");
             t1.Start();
             t2.Start();
+            t2.Join();
+            CheckVictory();
         }
         
-    }
-    
-    static void Go()
-    {
-        List<string> list = ["Piedra", "Papel", "Tijeras"];
-        string texto1 = list[Random.Range(0, list.Count)];
-        Console.WriteLine (texto1);
-    }
-    
-    static void Go2()
-    {
-        List<string> list = ["Piedra", "Papel", "Tijeras"];
-        string texto2 = list[Random.Range(0, 2)];
-        Console.WriteLine (texto2);
+        if (v1 > v2) Console.WriteLine("Este juego lo ha ganado el hilo 1");
+        else if (v1 < v2) Console.WriteLine("Este juego lo ha ganado el hilo 2");
+        else Console.WriteLine("Este juego ha quedado en empate");
     }
 
-    void CheckVictory()
+    static void Go()
+    {
+        var random = new Random();
+        List<string> list = ["Piedra", "Papel", "Tijeras"];
+        var text = list[random.Next(0, 3)];
+        texto1 = text;
+        Console.WriteLine ($"Hilo 1: {text}");
+        Thread.Sleep(500);
+    }
+
+    static void Go2()
+    {
+        var random = new Random();
+        List<string> list = ["Piedra", "Papel", "Tijeras"];
+        var text = list[random.Next(0, 3)];
+        texto2 = text;
+        Console.WriteLine ($"Hilo 2: {text}");
+        Thread.Sleep(600);
+    }
+
+    static void CheckVictory()
     {
         if (texto1 == "Tijeras" && texto2 == "Papel")
         {
@@ -63,7 +76,10 @@ class PiedraPapelTijeras
         {
             v2++;
             Console.WriteLine ("Ha ganado el hilo 2");
-        } 
-        
+        }
+        else
+        {
+            Console.WriteLine ("Empate");
+        }
     }
 }
---
+++ Thread1.cs
@@ -0,0 +1,7 @@
+﻿namespace PiedraPapelTijeras;
+
+public class Thread1():ThreadStart()
+{
+    public int v1 = 0;
+    public string text1 = "";
+}
